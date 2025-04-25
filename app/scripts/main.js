60bb44e3fe093f3a7539d091352ef59d1dccd83c
AmjadSoufi <113971567+AmjadSoufi@users.noreply.github.com>
2025-04-25 14:56:43 +0200

    added an arr for the the images

---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+const cards = [
+  { name: 'crying-cat', img: './img/crying-cat-meme.webp' },
+  { name: 'futurama', img: './img/futurama.jpg' },
+  { name: 'disaster-girl', img: './img/disaster-girl.jpg' },
+  { name: 'everywhere', img: './img/everywhere.jpg' },
+  { name: 'think', img: './img/think.jpg' },
+  { name: 'pablo-escobar', img: './img/pablo-escobar.jpg' }
+];
+
